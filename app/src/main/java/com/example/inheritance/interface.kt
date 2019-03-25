17178e46c97c7afdfbc6e6c2c11de862bc20384d
Zenzus <jonas.hein25@gmail.com>
2019-03-25 15:08:36 +0100

    interface stuff

--- app/src/main/java/com/example/inheritance/interface.kt
+++ app/src/main/java/com/example/inheritance/interface.kt
@@ -1,29 +1,26 @@
 package com.example.inheritance
 
-open class Herbivore (){
-    open fun eat(){println ("i eat plants")
-    }
+
+open class Herbivore(val age:Int) {
+    open fun eat(){println ("i eat plants")}
+
 }
 
-interface Carnivore {
+interface Carnivore{
     fun eat() { println("i eat meat") }
 
 }
 
-class Omnivore() : Herbivore(), Carnivore {
-    // The compiler requires f() to be overridden:
+open class Omnivore(age:Int,name: String) : Herbivore(age), Carnivore {
     override fun eat() {
         super<Herbivore>.eat() // call to A.f()
         super<Carnivore>.eat() // call to B.f()
     }
 }
 
-
 fun main(args: Array<String>) {
-    val human = Omnivore()
+    val human = Omnivore(21,"Jonas")
     human.eat()
-
-
 }
 
 
